51aa3beb25f7587499efeaaacc3b85b070dcdcd9
= <=>
2017-12-27 16:11:14 +0200

    remove deh tests

--- tests/testthat/test_get_functions.R
+++ tests/testthat/test_get_functions.R
@@ -17,7 +17,6 @@ test_that("get_ functions give errors", {
 test_that("get_ functions give warnings", {
   expect_warning(get_coords("kyy", "a"))
   expect_warning(get_timeseries("emy", "a"))
-  expect_warning(get_data("deh", "a"))
 })
 
 test_that("get_ functions return dataframes", {
@@ -25,9 +24,7 @@ test_that("get_ functions return dataframes", {
   # return data franes
   expect_is(get_stations("ypaat"), "data.frame")
   expect_is(get_coords("kyy", 10004), "data.frame")
-  expect_is(get_coords("deh", 30022), "data.frame")
   expect_is(get_timeseries("kyy", 200206), "data.frame")
-  expect_is(get_timeseries("deh", 30022), "data.frame")
 
   expect_is(get_timeseries("emy", 20100), "data.frame")
   expect_is(get_data("kyy", 789), "data.frame")
